3f26fffcea35348c2bbd3a0ab7bbb6dd22c774b8
James McFarland <24841376+jamesmcfarland@users.noreply.github.com>
2023-07-11 16:48:02 +0100

    Refactored datatable fetching into datatableHandler and getting org name

--- app/dashboard/[id]/page.tsx
+++ app/dashboard/[id]/page.tsx
@@ -7,19 +7,17 @@ import { cookies } from "next/headers";
 
 const Dashboard = async ({ params }: { params: { id: string } }) => {
   const supabase = createServerComponentClient({ cookies });
-
   const { data, error } = await supabase.functions.invoke(
-    "getMembersFromOrgId",
+    "getOrgNameFromOrgId",
     {
       body: { id: params.id },
     }
   );
-
-  if (error) {
-    console.warn(error);
-  }
-  if (data) {
-    console.log(data);
+  let organisationName;
+  if (!error) {
+    if (data.length) {
+      organisationName = data[0].organisationname;
+    }
   }
 
   return (
@@ -27,14 +25,16 @@ const Dashboard = async ({ params }: { params: { id: string } }) => {
       <nav className="flex flex-row justify-between py-2 px-4  border-b-2 items-center">
         <h2>socmanager</h2>
         <h1>Dashboard</h1>
-        <h2>ID: {params.id}</h2>
+        <h2>
+          {organisationName} ({params.id})
+        </h2>
         <div className="flex flex-row justify-between gap-x-2">
           <ModeToggle />
           <UserButton />
         </div>
       </nav>
 
-      <DataTableHandler data={data} />
+      <DataTableHandler id={params.id} />
     </div>
   );
 };
--- components/datatableHandler.tsx
+++ components/datatableHandler.tsx
@@ -1,6 +1,23 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { DataTableDemo } from "./datatable";
+import { cookies } from "next/headers";
 
-const DataTableHandler = ({ data }: { data: any }) => {
+const DataTableHandler = async ({ id }: { id: any }) => {
+  const supabase = createServerComponentClient({ cookies });
+  const { data, error } = await supabase.functions.invoke(
+    "getMembersFromOrgId",
+    {
+      body: { id: id },
+    }
+  );
+
+  if (error) {
+    console.warn(error);
+  }
+  if (data) {
+    console.log(data);
+  }
+
   if (data.length > 0) {
     return <DataTableDemo data={data} type={data[0].organisationtype} />;
   } else {
